03ecda1a87824d5e3c3371480658fc7cecd075b5
Verona <verona.m@outlook.de>
2022-03-20 04:50:35 +0100

    gets Restaurants, Hotels and Attractions nearby

--- src/App.js
+++ src/App.js
@@ -14,15 +14,13 @@ const App = () => {
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude}}) =>{
-            setCoordinates({lat: latitude, lng: longitude});
+            setCoordinates({ lat: latitude, lng: longitude});
         })
-    },[])
+    },[]);
 
     useEffect(() => {
-        getPlacesData()
+        getPlacesData(bounds.sw, bounds.ne)
             .then((data) => {
-                console.log(data);
-
                 setPlaces(data);
             })
     },[coordinates, bounds]);
@@ -33,7 +31,7 @@ const App = () => {
             <Header />
             <Grid container spacing={3} style={{width: '100%'}}>
                 <Grid item xs={12} md={4}>
-                    <List />
+                    <List places={places}/>
                 </Grid>
                 <Grid item xs={12} md={8}>
                    <Map 
