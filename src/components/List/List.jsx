d1beabc7af979ee6bf4165d601349546d9782829
Verona <verona.m@outlook.de>
2022-03-27 23:44:58 +0200

    corrected errors and functional filtering by type and rating

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,19 +1,29 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, createRef} from 'react'
 import { CircularProgress, Grid, Typography, Inputlabel, MenuItem, FormControl, Select, InputLabel} from '@material-ui/core'
 
 import PlaceDetails from '../PlaceDetails/PlaceDetails';
 import useStyles from './styles'
 
-const List = ({ places }) => {
+const List = ({ places, childClicked, isLoading, type, setType, rating, setRating}) => {
   const classes = useStyles();
-  const[type, setType] = useState('restaurants');
-  const[rating, setRating] =useState('');
+  const [elRefs, setElRefs] = useState([]);
 
+
+  useEffect(() => {
+    const refs = Array(places?.length).fill().map((_, i) => elRefs[i] || createRef());
+
+    setElRefs(refs);
+  }, [places])
 
   return (
     <div className={classes.container}>
       <Typography variant="h4">Restaurants, Hotels & Attractions around you</Typography>
-
+      {isLoading ? (
+        <div className={classes.loading}>
+          <CircularProgress size="5rem" />
+        </div>
+      ) : (
+        <>
       <FormControl className={classes.formControl}>
         <InputLabel>Type</InputLabel>
         <Select value={type} onChange={(e) => setType(e.target.value)}>
@@ -34,11 +44,17 @@ const List = ({ places }) => {
       </FormControl>
       <Grid container spacing={3} className={classes.list}>
         {places?.map((place, i) => (
-          <Grid item key={i} xs={12}>
-              <PlaceDetails place={place}/>
+          <Grid ref={elRefs[1]} item key={i} xs={12}>
+              <PlaceDetails 
+              place={place}
+              selected={Number(childClicked) === i}
+              refProp={elRefs[i]}
+              />
           </Grid>
         ))}
       </Grid>
+      </>
+      )}
     </div>
   )
 }
